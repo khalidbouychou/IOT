760df2bf908c47caa70bca6189319e2e0205ad75
Khalid Bouychou <khbouych@e3r10p12.1337.ma>
2026-05-10 20:16:28 +0100

    stiil

--- bonus/scripts/setup-cluster-namespaces.sh
+++ bonus/scripts/setup-cluster-namespaces.sh
@@ -1,9 +1,35 @@
-#!/bin/sh
+#!/bin/bash
 
-k3d cluster create bonuscluster -p "8080:80@loadbalancer" -p "8888:8888@loadbalancer"
+# --- 1. SET ENVIRONMENT ---
+export PATH="/goinfre/$(whoami)/bin:$PATH"
+GOINFRE="/goinfre/$(whoami)"
+export PATH="$PATH:$GOINFRE/bin"
 
-kubectl create namespace argocd
-kubectl create namespace dev
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-
+k3d cluster create bonus-clr -p "8080:80@loadbalancer" -p "8888:8888@loadbalancer" --wait
 kubectl create namespace gitlab
+
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl create namespace argocd
+# --- 2. GET GITLAB PASSWORD ---
+echo "--- 🔑 Retrieving GitLab Root Password ---"
+GITLAB_PASS=$(vagrant ssh gitlab-server -c "sudo cat /etc/gitlab/initial_root_password" | grep "Password:" | awk '{print $2}' | tr -d '\r')
+PASS=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
+
+echo "Password found: $GITLAB_PASS"
+echo "---------------------Gitlab Cpanel---------------------------------------"
+echo "✅ BONUS PART IS READY"
+echo "GitLab URL: http://192.168.56.120"
+echo "GitLab User: root"
+echo "GitLab Pass: $GITLAB_PASS"
+echo "------------------------AgroCD------------------------------------"
+echo "ArgoCD URL: http://192.168.56.120"
+echo "ArgoCD User: admin"
+echo "ArgoCD Pass: $PASS"
+echo "------------------------------------------------------------"
+echo "after creating repo run these commands : 
+-  vagrant suspend gitlab-server \n
+- k3d kubeconfig merge bonus-clr  --kubeconfig-switch-context \n
+- kubectl apply -f confs/application-bonus.yaml \n
+- vagrant resume gitlab-server \n
+- kubectl port-forward -n argocd svc/argocd-server 8080:443
+"
--- bonus/scripts/setup.sh
+++ bonus/scripts/setup.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
+
+
+
 export DEBIAN_FRONTEND=noninteractive
-
-# 1. Install Dependencies
-apt-get update
-apt-get install -y curl openssh-server ca-certificates tzdata perl
-
-# 2. Add GitLab Repository and Install
-curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | bash
-# EXTERNAL_URL is crucial so GitLab knows its IP
+sudo apt-get update && sudo apt-get install -y curl openssh-server ca-certificates tzdata perl
+curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
 sudo EXTERNAL_URL="http://192.168.56.120" apt-get install -y gitlab-ce
-
-# 3. Optimize for 8GB iMac (Disable heavy features)
-# This saves about 1.5GB of RAM
-cat <<EOF >> /etc/gitlab/gitlab.rb
-puma['worker_processes'] = 0
-sidekiq['max_concurrency'] = 5
-prometheus['enable'] = false
-grafana['enable'] = false
-alertmanager['enable'] = false
-EOF
-
-# 4. Apply settings
-gitlab-ctl reconfigure
+# Optimization for 8GB RAM
+echo "prometheus['enable'] = false" | sudo tee -a /etc/gitlab/gitlab.rb
+echo "puma['worker_processes'] = 0" | sudo tee -a /etc/gitlab/gitlab.rb
+sudo gitlab-ctl reconfigure
