b77ffb3cd4c405dc4f06af11f8d98e5a99c121be
khalid bouychou <khalidbouychou22@gmail.com>
2026-05-19 13:24:53 +0100

    fix p1

--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-# Install Master node
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip 192.168.56.110 --write-kubeconfig-mode 644" sh -
-# Export the token so the worker can use it
-sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant/scripts/node-token
+
+sudo apt-get update -y
+sudo apt-get install -y net-tools curl
+
+ # Install K3s server
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip 192.168.56.110 --tls-san 192.168.56.110" sh -
+  # Replace the server IP in the kubeconfig file
+sudo sed -i 's/127.0.0.1/192.168.56.110/' /etc/rancher/k3s/k3s.yaml
+ # Configure kubectl access
+sudo  echo 'export KUBECONFIG=/etc/rancher/k3s/k3s.yaml' >> /root/.bash 
+ # Get node token
+sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant/node-token
--- p1/scripts/worker.sh
+++ p1/scripts/worker.sh
@@ -1,6 +1,11 @@
-#!/bin/bash
-# Wait for master to generate the token
-while [ ! -f /vagrant/scripts/node-token ]; do sleep 2; done
-# Install Worker node pointing to Master IP
-K3S_TOKEN=$(cat /vagrant/scripts/node-token)
-curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$K3S_TOKEN INSTALL_K3S_EXEC="--node-ip 192.168.56.111" sh -
+  #!/bin/bash
+  sudo apt-get update -y
+  sudo apt-get install -y net-tools curl
+  # Wait until token exists
+  while [ ! -f /vagrant/node-token ]
+  do
+    sleep 2
+  done
+  TOKEN=$(cat /vagrant/node-token | tr -d '\n')
+  # Install K3s agent
+  sudo curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$TOKEN sh -
